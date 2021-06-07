2f07be2d2bf38ccaa7a3ac8af56ae8107ea35200
TurtlePaw <turtlesimsyt@gmail.com>
2021-06-07 19:25:30 -0400

    -

---
+++ cmds/test-cmd.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const wait = require('util').promisify(setTimeout);
+
+module.exports = {
+ name: "test",
+ description: "Returns latency and API ping",
+ async execute(message, Member, args) {
+     
+ },
+};
--- slash/docs.js
+++ slash/docs.js
@@ -7,25 +7,26 @@ module.exports = {
     async execute(client, interaction) {
         async function fetchDocs(term) {
             const q = encodeURIComponent(term.toString());
-        
-            const res = await fetch(`https://djsdocs.sorta.moe/v1/main/${interaction.options?.find(c => c?.name === 'source').value ?? 'stable'}/embed?q=${q}`);
+            const source = interaction.options?.find(c => c?.name === 'source')?.value ?? 'stable'
+            const res = await fetch(`https://djsdocs.sorta.moe/v1/main/${source}/embed?q=${q}`);
             const embed = await res.json();
         
             return embed; 
         };
-
-        fetchDocs(`${interaction.options?.find(c => c?.name === 'query').value}`).then(async (docr) => {
+        const search = interaction.options?.find(c => c?.name === 'query').value
+        fetchDocs(`${search}`).then(async (docr) => {
                 const docs = new Discord.MessageEmbed()
                 .setAuthor(`${docr.author?.name}`, `${docr.author?.icon_url}`, `${docr.author?.url}`)
                 .setTitle(`Search results:`)
                 .setDescription(docr.description)
                 .setColor('BLUE')
                 await interaction.reply(docs);
+                const userr = interaction.options?.find(c => c?.name === 'target').user;
                 if(interaction.options?.find(c => c?.name === 'target')){
-                await interaction.followUp(`${interaction.options?.find(c => c?.name === 'target').user}`).then(async (msg) => {
+                await interaction.followUp(`${userr}`).then(async (msg) => {
                 setTimeout(async () => {
                     await msg.delete()
-                }, 4000);
+                }, 1000);
                 }
                 )
                 }
